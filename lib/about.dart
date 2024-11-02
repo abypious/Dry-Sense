cd3b60b0a0a8b8ace28637523938f3781a9ea0b3
athira vijayan <athirapunnekkad@gmail.com>
2024-11-02 05:38:40 +0530

    ESP32 to NodeMCU

--- lib/about.dart
+++ lib/about.dart
@@ -17,19 +17,19 @@ class AboutScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'IoT Based Cloth Drying Monitoring System with ESP32 and Moisture Sensor',
+                  'IoT Based Cloth Drying Monitoring System with NodeMCU and Moisture Sensor',
                   style: Theme.of(context).textTheme.titleMedium?.copyWith(
                     fontWeight: FontWeight.bold,
                     fontSize: 22,
-                  ), // Bold title with larger font size
+                  ),
                   textAlign: TextAlign.justify,
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 20),
                 const Text(
                   'The Dry Sense project is an Internet of Things (IoT) based solution designed to monitor and determine whether cloth is dry or not. This system utilizes IoT devices, sensors, and cloud connectivity to provide real-time data, helping users manage the drying process effectively.',
                   textAlign: TextAlign.justify,
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 20),
                 Text(
                   'Project Overview',
                   style: const TextStyle(
@@ -39,16 +39,15 @@ class AboutScreen extends StatelessWidget {
                   textAlign: TextAlign.justify,
                 ),
                 const Text(
-                  'This project addresses the common challenge of ensuring that cloth is adequately dried, preventing dampness and associated issues such as mold growth and odor. The system employs an ESP32 microcontroller as the central processing unit, interfacing with various components to monitor moisture levels in the cloth.',
+                  'This project addresses the common challenge of ensuring that cloth is adequately dried, preventing dampness and associated issues such as mold growth and odor. The system employs a NodeMCU microcontroller as the central processing unit, interfacing with various components to monitor moisture levels in the cloth.',
                   textAlign: TextAlign.justify,
                 ),
                 const SizedBox(height: 40),
                 Column(
                   children: [
-                    Image.asset('assets/ESP32.jpg'),
-                    const SizedBox(height: 40),
+                    Image.asset('assets/NodeMCU.jpg'), // Updated image
                     Text(
-                      'ESP32 Microcontroller',
+                      'NodeMCU Microcontroller',
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -56,7 +55,7 @@ class AboutScreen extends StatelessWidget {
                       textAlign: TextAlign.justify,
                     ),
                     const Text(
-                      'The ESP32 is a versatile, Wi-Fi-enabled microcontroller that serves as the core processing unit of the Dry Sense system. It connects to the moisture sensor to receive real-time data on moisture levels and displays this information. The ESP32 also manages the system\'s connectivity with the cloud or a mobile app for remote monitoring and notifications.',
+                      'The NodeMCU is a versatile, Wi-Fi-enabled microcontroller that serves as the core processing unit of the Dry Sense system. It connects to the moisture sensor to receive real-time data on moisture levels and displays this information. The NodeMCU also manages the system\'s connectivity with the cloud or a mobile app for remote monitoring and notifications.',
                       textAlign: TextAlign.justify,
                     ),
                     const SizedBox(height: 40),
@@ -84,10 +83,10 @@ class AboutScreen extends StatelessWidget {
                       textAlign: TextAlign.justify,
                     ),
                     const Text(
-                      'The display, connected to the ESP32, shows the current moisture levels in real-time, giving users immediate feedback on the cloth\'s dryness status. This component can be an OLED or LCD screen, depending on requirements, and displays moisture levels in a user-friendly format.',
+                      'The display, connected to the NodeMCU, shows the current moisture levels in real-time, giving users immediate feedback on the cloth\'s dryness status. This component can be an OLED or LCD screen, depending on requirements, and displays moisture levels in a user-friendly format.',
                       textAlign: TextAlign.justify,
                     ),
-                    const SizedBox(height: 40),
+                    const SizedBox(height: 20),
                     Text(
                       'Connecting Wires',
                       style: const TextStyle(
@@ -97,10 +96,10 @@ class AboutScreen extends StatelessWidget {
                       textAlign: TextAlign.justify,
                     ),
                     const Text(
-                      'Wires are used to establish connections between the ESP32, moisture sensor, and display. These connections facilitate data flow between components, allowing the ESP32 to collect moisture data and communicate results to the display and cloud interface.',
+                      'Wires are used to establish connections between the NodeMCU, moisture sensor, and display. These connections facilitate data flow between components, allowing the NodeMCU to collect moisture data and communicate results to the display and cloud interface.',
                       textAlign: TextAlign.justify,
                     ),
-                    const SizedBox(height: 40),
+                    const SizedBox(height: 20),
                     Text(
                       'Power Supply',
                       style: const TextStyle(
@@ -115,7 +114,7 @@ class AboutScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 20),
                 Text(
                   'Functionality',
                   style: const TextStyle(
@@ -128,7 +127,7 @@ class AboutScreen extends StatelessWidget {
                   'The Dry Sense system can operate in both manual and automatic modes. In automatic mode, the system provides alerts or notifications when the cloth reaches a predefined dryness level, helping users optimize their drying processes. In manual mode, users can check moisture levels directly via the display.',
                   textAlign: TextAlign.justify,
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 20),
                 Text(
                   'User Interaction',
                   style: const TextStyle(
@@ -141,7 +140,7 @@ class AboutScreen extends StatelessWidget {
                   'Users can monitor moisture levels and receive notifications through a mobile application, such as the Blynk app, ensuring they have easy access to important information regarding their drying processes. This setup promotes efficiency in managing cloth drying, minimizing energy usage, and preventing waste.',
                   textAlign: TextAlign.justify,
                 ),
-                const SizedBox(height: 40),
+                const SizedBox(height: 20),
                 Text(
                   'Conclusion',
                   style: const TextStyle(
@@ -151,7 +150,7 @@ class AboutScreen extends StatelessWidget {
                   textAlign: TextAlign.justify,
                 ),
                 const Text(
-                  'The Dry Sense project exemplifies an innovative approach to managing drying processes using IoT technology. By integrating a moisture sensor with the ESP32 and providing real-time monitoring through a display and mobile app, this system enhances user experience and promotes effective cloth management.',
+                  'The Dry Sense project exemplifies an innovative approach to managing drying processes using IoT technology. By integrating a moisture sensor with the NodeMCU and providing real-time monitoring through a display and mobile app, this system enhances user experience and promotes effective cloth management.',
                   textAlign: TextAlign.justify,
                 ),
                 const SizedBox(height: 40),
@@ -159,7 +158,7 @@ class AboutScreen extends StatelessWidget {
                   'Version: 1.0.0',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                    fontSize: 10,
                   ),
                 ),
               ],
